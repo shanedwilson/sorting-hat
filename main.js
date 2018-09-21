2a3322076c2de1f9450bf0d53c5f98b14d7b9158
Shane D Wilson <me@shanedwilson.com>
2018-09-21 07:58:49 -0500

    wip

--- main.js
+++ main.js
@@ -1,48 +1,46 @@
 const houses = [
     {
         name:'Gryffindor', 
-        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/8/8e/0.31_Gryffindor_Crest_Transparent.png/revision/latest?cb=20161124074004'
+        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/8/8e/0.31_Gryffindor_Crest_Transparent.png/revision/latest/scale-to-width-down/700?cb=20161124074004'
     },
 
     {
         name: 'Hufflepuff',
-        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/5/50/0.51_Hufflepuff_Crest_Transparent.png/revision/latest?cb=20161020182518'
+        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/5/50/0.51_Hufflepuff_Crest_Transparent.png/revision/latest/scale-to-width-down/700?cb=20161020182518'
     },
 
     {
         name: 'Ravenclaw',
-        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/2/29/0.41_Ravenclaw_Crest_Transparent.png/revision/latest?cb=20161020182442'
+        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/2/29/0.41_Ravenclaw_Crest_Transparent.png/revision/latest/scale-to-width-down/700?cb=20161020182442'
     },
 
     {
         name: 'Slytherin',
-        image: 'http://cdn.playbuzz.com/cdn/204cdd30-3172-4631-8d23-b6af7f63401e/5aa52373-6c49-4544-8eee-f0a4f5ec1eed.jpg'
+        image: 'https://vignette.wikia.nocookie.net/harrypotter/images/d/d3/0.61_Slytherin_Crest_Transparent.png/revision/latest/scale-to-width-down/700?cb=20161020182557'
     }
 ];
 
 const studentNameElem = document.getElementById('student-name');
-const studentName = studentNameElem.value;
 const sortButtonElem = document.getElementById('sort-button');
 const startButtonElem = document.getElementById('start-button');
 const jumbotron = document.getElementById('jumbo');
 const nameInput = document.getElementById('name-input');
 const expelButtonElem = document.getElementById('expel');
 
-
-
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
 
-const buildNewStudentCard = (studentName) => {
+const buildNewStudentCard = () => {
+    const studentName = studentNameElem.value;
     let domString = '';
     let randomize = Math.floor((Math.random() * houses.length));
     let house = houses[randomize].name;
     let houseImg = houses[randomize].image;
-    domString += `<div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${houseImg}" alt="">
-        <div class="card-body">
+    domString += `<div class="card d-flex row justify-content-center m-2" style="width: 18rem;">
+        <img class="card-img-top" src="${houseImg}" alt="${house}">
+        <div class="card-body text-center">
             <h5 class="card-name">${studentName}</h5>
             <h6 class="card-house">${house}</p>
             <button type="button" class="expel-button btn btn-danger" id="expel">EXPEL</button>
@@ -67,7 +65,7 @@ const activateExpel = () => {
 
 sortButtonElem.addEventListener('click', (e) => {
     e.preventDefault();
-    buildNewStudentCard(studentName);
+    buildNewStudentCard();
 });
 
 startButtonElem.addEventListener('click', (e) => {
