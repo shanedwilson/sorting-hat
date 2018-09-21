84346f5ef006a0363e4c7bf911e78b30caba5932
Shane D Wilson <me@shanedwilson.com>
2018-09-20 19:35:16 -0500

    added random number generator

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = stringToPrint;
 };
+
+let randomize = Math.floor((Math.random() * houses.length));
+let house ='';
